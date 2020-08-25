04df97be734a6c8e085959e1634ecb8c8458ca25
Ethan <2drewwilson@gmail.com>
2020-08-25 15:29:26 -0400

    finished v.1

---
+++ index.js
@@ -0,0 +1,22 @@
+var countDownDate = new Date("Sept 10, 2020 10:00:00 EST").getTime();
+
+var x = setInterval(function () {
+
+    
+    var now = new Date().getTime();
+
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("fallcountdown").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("fallcountdown").innerHTML = "EXPIRED";
+    }
+}, 1000);
